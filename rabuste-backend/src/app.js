30816868bee2a3a23ca395386fd8c90794f6ae80
sam <bolisettysamanvitha@gmail.com>
2025-12-21 19:09:38 +0530

    added app.js ,modified server.js ,modified backend file structure

--- rabuste-backend/src/app.js
+++ rabuste-backend/src/app.js
@@ -1,21 +1,18 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-import connectDB from "./db.js";
+
 import authRoutes from "./routes/auth.js";
 import protectedRoutes from "./routes/protected.js";
 
-dotenv.config();
-connectDB();
-
 const app = express();
 
+// logger
 app.use((req, res, next) => {
   console.log("➡️", req.method, req.url);
   next();
 });
 
-
+// middleware
 app.use(cors({
   origin: "http://localhost:3000",
   allowedHeaders: ["Content-Type", "Authorization"],
@@ -23,14 +20,13 @@ app.use(cors({
 
 app.use(express.json());
 
+// test route
 app.get("/", (req, res) => {
   res.send("Backend running");
 });
 
+// routes
 app.use("/api/auth", authRoutes);
 app.use("/api/protected", protectedRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Backend running on port ${PORT}`);
-});
+export default app;
