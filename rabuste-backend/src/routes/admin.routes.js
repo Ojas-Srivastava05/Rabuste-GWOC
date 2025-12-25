33d8e7a66653576b7914bc71e1411255a936626c
samanvitha7 <bolisettysamanvitha@gmail.com>
2025-12-25 12:06:06 +0530

    modified admn routes

--- rabuste-backend/src/routes/admin.routes.js
+++ rabuste-backend/src/routes/admin.routes.js
@@ -11,6 +11,8 @@ router.get("/menu/:id", auth, admin, menu.getMenuItemById);
 router.put("/menu/:id", auth, admin, menu.updateMenuItem);
 router.delete("/menu/:id", auth, admin, menu.deleteMenuItem);
 router.get("/orders",auth,admin,getAllOrders);
+router.patch( "/orders/:id/slot",auth,admin,assignPickupSlot);
+    
   
 
 export default router;
